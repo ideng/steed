0bed35b7c21a43592c67d36c11b6f786463fabaf
hui.deng <hui.deng@58-PC>
2014-11-28 17:29:44 +0800

    httpclient update

--- steed-common/src/main/java/com/ideng/steed/common/exceptions/SteedError.java
+++ steed-common/src/main/java/com/ideng/steed/common/exceptions/SteedError.java
@@ -8,12 +8,21 @@ package com.ideng.steed.common.exceptions;
  */
 public class SteedError {
 
-    protected static SteedException throwError(ErrorStatus error,  String msg)
+    protected static SteedException throwError(ErrorStatus error, String msg)
     {
         return new SteedException(error, msg);
     }
     
+    protected static SteedException throwError(ErrorStatus error, String msg, Exception inner)
+    {
+        return new SteedException(error, msg, inner);
+    }
+    
     public static SteedException Unknown() {
-        return throwError(ErrorStatus.ERROR_UNKNOWN, "Internal Error");
+        return throwError(ErrorStatus.UNKNOWN, "internal error");
+    }
+    
+    public static SteedException ExecutionError(Exception inner) {
+        return throwError(ErrorStatus.EXECUTION_FAILED, "execution failed", inner);
     }
 }
