8d9d34d863b665e1c4ecd0b05e882b8707f796e8
mdeng <tianma_25@163.com>
2015-04-25 17:15:29 +0800

    top 3 pages

--- serank/src/main/java/com/mdeng/serank/KeywordRank.java
+++ serank/src/main/java/com/mdeng/serank/KeywordRank.java
@@ -1,13 +1,9 @@
 package com.mdeng.serank;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
 import com.mdeng.serank.spiders.GrabResult;
 
-public class KeywordRank {
+public abstract class KeywordRank {
   private String keyword;
-  private List<RankInfo> ranks = Lists.newArrayList();
   private GrabResult result = GrabResult.FAILED;
 
   public String getKeyword() {
@@ -18,14 +14,6 @@ public class KeywordRank {
     this.keyword = keyword;
   }
 
-  public List<RankInfo> getRanks() {
-    return ranks;
-  }
-
-  public void setRanks(List<RankInfo> ranks) {
-    this.ranks = ranks;
-  }
-
   public GrabResult getResult() {
     return result;
   }
@@ -34,34 +22,4 @@ public class KeywordRank {
     this.result = result;
   }
 
-  class RankInfo {
-    private String host;
-    private String address;
-    private int rank;
-
-    public String getHost() {
-      return host;
-    }
-
-    public void setHost(String host) {
-      this.host = host;
-    }
-
-    public String getAddress() {
-      return address;
-    }
-
-    public void setAddress(String address) {
-      this.address = address;
-    }
-
-    public int getRank() {
-      return rank;
-    }
-
-    public void setRank(int rank) {
-      this.rank = rank;
-    }
-
-  }
 }
