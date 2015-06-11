8cbb7d111cace67a3b7cceab1e012d2a7a7b1157
Deng <tianma_25@163.com>
2015-06-11 13:12:08 +0800

    refactor

--- common/src/main/java/com/mdeng/common/http/HttpRequestBuilder.java
+++ common/src/main/java/com/mdeng/common/http/HttpRequestBuilder.java
@@ -1,11 +1,5 @@
 package com.mdeng.common.http;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -17,8 +11,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.mdeng.common.utils.Jsons;
 
 /**
  * To build a HTTP request.
@@ -88,63 +80,4 @@ public class HttpRequestBuilder {
     return null;
   }
 
-
-  /**
-   * Get status line from http response header.
-   * 
-   * @author hui.deng
-   *
-   */
-  public static class StatusLineHandler implements ResponseHandler<StatusLine> {
-
-    public StatusLine handleResponse(HttpResponse response) throws ClientProtocolException,
-        IOException {
-      return response.getStatusLine();
-    }
-
-  }
-
-  /**
-   * JsonEntityHandler
-   * 
-   * @author hui.deng
-   *
-   * @param <T>
-   */
-  public static class JsonEntityHandler<T> implements ResponseHandler<T> {
-
-    private Class<T> clazz;
-
-    public JsonEntityHandler(Class<T> clazz) {
-      this.clazz = clazz;
-    }
-
-    @Override
-    public T handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-      try {
-        InputStream in = response.getEntity().getContent();
-        return Jsons.json2Obj(in, clazz);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-
-      return null;
-    }
-
-  }
-
-  /**
-   * String entity handler
-   * 
-   * @author hui.deng
-   *
-   */
-  public static class StringEntityHandler implements ResponseHandler<String> {
-
-    @Override
-    public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
-      return EntityUtils.toString(response.getEntity());
-    }
-
-  }
 }
