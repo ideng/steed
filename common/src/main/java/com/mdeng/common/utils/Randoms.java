bc6541e40df36616bed17d5d819cb5f3a93faee2
Deng <tianma_25@163.com>
2015-06-25 10:23:59 +0800

    support excludes

--- common/src/main/java/com/mdeng/common/utils/Randoms.java
+++ common/src/main/java/com/mdeng/common/utils/Randoms.java
@@ -9,6 +9,8 @@ import java.util.Set;
 
 import org.apache.http.util.Args;
 
+import com.google.common.collect.Lists;
+
 /**
  * Random utilities
  * 
@@ -30,6 +32,43 @@ public class Randoms {
     if (col.size() == 0) return col;
 
     List<E> list = new ArrayList<E>(col);
+    Collections.shuffle(list);
+
+    Collection<E> ret;
+    if (col instanceof List) {
+      ret = new ArrayList<E>(limit);
+    } else if (col instanceof Set) {
+      ret = new HashSet<E>(limit);
+    } else {
+      throw new IllegalArgumentException("unsupported collection");
+    }
+
+    for (E e : list) {
+      ret.add(e);
+      if (ret.size() >= limit) break;
+    }
+
+    return ret;
+  }
+
+  /**
+   * Random select limit counts from collection, but excludes specified elements
+   * 
+   * @param col
+   * @param limit
+   * @param excludes
+   * @return
+   */
+  public static <E> Collection<E> rand(Collection<E> col, int limit, E... excludes) {
+    Args.notNull(col, "col");
+    Args.positive(limit, "limit");
+    if (col.size() == 0) return col;
+
+    List<E> list = new ArrayList<E>();
+    List<E> excludesLst = Lists.newArrayList(excludes);
+    for (E e : col) {
+      if (!excludesLst.contains(e)) list.add(e);
+    }
     Collections.shuffle(list);
 
     Collection<E> ret;
