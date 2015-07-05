69ea2e1fa365208f2dd129397500a7014eaa2c98
mdeng <tianma_25@163.com>
2015-07-05 16:13:03 +0800

    exception

---
+++ oceanusex/src/main/java/com/mdeng/oceanusex/exceptions/DuplicateException.java
@@ -0,0 +1,10 @@
+package com.mdeng.oceanusex.exceptions;
+
+public class DuplicateException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -691260359404054888L;
+
+}
---
+++ oceanusex/src/main/java/com/mdeng/oceanusex/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.mdeng.oceanusex.exceptions;
+
+public class NotFoundException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 9066658057127382806L;
+
+}
